c2a58ce725d6bc75febef3af359641dc9a331be9
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-16 14:11:09 -0500

    first commit

---
+++ src/main/java/com/example/BlogCast/BlogCastApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlogCast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCastApplication.class, args);
+	}
+
+}
