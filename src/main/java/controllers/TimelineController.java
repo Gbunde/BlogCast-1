bf12798045bfbccd5987160ba2ca690ce74b1d39
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-17 14:53:19 -0500

    added to timeline

--- src/main/java/controllers/TimelineController.java
+++ src/main/java/controllers/TimelineController.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import services.BlogEntryService;
 
 @RestController
 public class TimelineController {
+    @Autowired
+
+    private BlogEntryService blogEntryService;
+
+
+
 }
--- src/main/java/repositories/TimelineRepository.java
+++ src/main/java/repositories/TimelineRepository.java
@@ -4,7 +4,10 @@ import models.Timeline;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TimelineRepository extends CrudRepository<Timeline, Long> {
+    List<Timeline> findTimelineBy(String blogEntry);
 
 }
