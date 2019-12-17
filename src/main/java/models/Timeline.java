b4258ecdab981ae894bec9b0e172a37fc2fd6c4f
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-17 15:26:58 -0500

    added to timeline

--- src/main/java/models/Timeline.java
+++ src/main/java/models/Timeline.java
@@ -5,25 +5,58 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 public class Timeline {
-
     @Id
     @GeneratedValue
-
+    private String title;
+    private Object body;
+    private Date timeStamp;
     private Long id;
     @OneToMany
     private List<BlogEntry> AllBlogs = new ArrayList<>();
+    private User author;
 
     public Timeline() {
     }
 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
     public Timeline(List<BlogEntry> allBlogs) {
         AllBlogs = (ArrayList<BlogEntry>) allBlogs;
     }
-
 
     public Long getId() {
         return id;
